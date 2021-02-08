3cf1770db82bd2bc4e1bab286d7bd12e8283a81a
rekaerst <rekaerst@outlook.com>
2021-02-09 07:37:00 +0800

    pyqt: arithmetic logic

--- pyqt/main.py
+++ pyqt/main.py
@@ -1,27 +1,60 @@
 #!/usr/bin/python
 
 import sys
+from enum import Enum, auto
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QVBoxLayout, QLineEdit
 
 
 class Calc(QWidget):
 
+    class Display(QLineEdit):
+        def __init__(self):
+            super().__init__()
+
+        def backspace(self):
+            if self.text() == "0":
+                pass
+            else:
+                super().backspace()
+
+        def insert(self, a0: str):
+            if a0 != ".":
+                if self.text() == "0":
+                    self.setText(a0)
+                else:
+                    super().insert(a0)
+            elif self.text().find(".") == -1:
+                super().insert(".")
+
+        def clear(self):
+            self.setText("0")
+
+    class Operation(Enum):
+        NUL = auto()
+        ADD = auto()
+        SUB = auto()
+        MUL = auto()
+        DIV = auto()
+        MOD = auto()
+
     def __init__(self):
         super().__init__()
-        self.display = QLineEdit()
+        self.display = Calc.Display()
+        self.accumulator = 0
+        self.operation = Calc.Operation.NUL
+        self.is_operation_button_clicked = False
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Calculator")
-        self.display.setText("I fart in your general direction")
+        self.display.clear()
 
         gridLayout = QGridLayout()
 
-        mainLayout = QVBoxLayout() 
+        mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.display)
         mainLayout.addLayout(gridLayout)
         self.setLayout(mainLayout)
-        
 
         buttonNames = ["C", "<-", "%", "/",
                        "7", "8", "9", "x",
@@ -34,10 +67,6 @@ class Calc(QWidget):
         for position, name in zip(buttonPositions, buttonNames):
             button = QPushButton(name)
 
-            if name == "=":
-                gridLayout.addWidget(button, 4, 2, 1, 2)
-                continue
-
             if name.isdigit():
                 button.clicked.connect(
                     lambda checked, arg=int(name): self.on_number_button_clicked(arg))
@@ -46,16 +75,63 @@ class Calc(QWidget):
                     lambda checked, arg=name: self.on_button_clicked(arg)
                 )
 
+            if name == "=":
+                gridLayout.addWidget(button, 4, 2, 1, 2)
+                continue
+
             gridLayout.addWidget(button, *position)
 
         self.setWindowTitle("Calculator")
         self.show()
 
-    def on_number_button_clicked(self, digit):
-        print(str(digit) + " clicked")
+    def calculate(self, operation):
+        if not self.is_operation_button_clicked:
+            if self.operation == Calc.Operation.ADD:
+                self.accumulator += float(self.display.text())
+                print(self.accumulator)
+            elif self.operation == Calc.Operation.SUB:
+                self.accumulator -= float(self.display.text())
+            elif self.operation == Calc.Operation.MUL:
+                self.accumulator *= float(self.display.text())
+            elif self.operation == Calc.Operation.DIV:
+                self.accumulator /= float(self.display.text())
+            elif self.operation == Calc.Operation.MOD:
+                self.accumulator %= float(self.display.text())
+            elif self.operation == Calc.Operation.NUL:
+                self.accumulator = float(self.display.text())
 
-    def on_button_clicked(self, name):
-        print(name + " clicked")
+        self.display.setText(str(self.accumulator))
+        self.is_operation_button_clicked = True
+        self.operation = operation
+
+    def on_number_button_clicked(self, digit: int):
+        if self.is_operation_button_clicked:
+            self.display.clear()
+            self.is_operation_button_clicked = False
+        self.display.insert(str(digit))
+
+    def on_button_clicked(self, name: str):
+        if name == "C":
+            self.accumulator = 0
+            self.is_operation_button_clicked = False
+            self.operation = Calc.Operation.NUL
+            self.display.clear()
+        elif name == "<-":
+            self.display.backspace()
+        elif name == "+":
+            self.calculate(Calc.Operation.ADD)
+        elif name == "-":
+            self.calculate(Calc.Operation.SUB)
+        elif name == "x":
+            self.calculate(Calc.Operation.MUL)
+        elif name == "/":
+            self.calculate(Calc.Operation.DIV)
+        elif name == "%":
+            self.calculate(Calc.Operation.MOD)
+        elif name == "=":
+            self.calculate(Calc.Operation.NUL)
+        elif name == ".":
+            self.display.insert(".")
 
 
 def main():
